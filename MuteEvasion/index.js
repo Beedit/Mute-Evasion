1be3eba96dfbe4514f25e2a7b921b621fdec3cb9
beedit <bgroulef@pm.me>
2022-12-08 14:05:16 +0000

    ;

--- MuteEvasion/index.js
+++ MuteEvasion/index.js
@@ -1,21 +1,20 @@
 import WebSocket from "WebSocket";
-import Settings from "./config"
+import Settings from "./config";
 
 
 let ws = new WebSocket(`ws://${Settings.host}:${Settings.port}`);
 
-let connected = false
-let unload = false
+let connected = false;
+let unload = false;
 
 function chat(message){
-    ChatLib.chat("&dMute Evade >>&f " + message)
+    ChatLib.chat("&dMute Evade >>&f " + message);
 }
 
 ws.onMessage = (msg) => {
-    msg = JSON.parse(msg)
+    msg = JSON.parse(msg);
     if (msg.method == "message"){
-        console.log(msg.data)
-        chat(msg.data)
+        chat(msg.data);
     }
 }
 
@@ -23,25 +22,25 @@ ws.onError = (exception) => { print("Error: " + exception); }
 
 ws.onOpen = () => { 
     console.log("Socket Opened");
-    chat("Connection established with the websocket.")
-    ws.send(JSON.stringify({method: "username", data: Player.getName()}))
+    chat("Connection established with the websocket.");
+    ws.send(JSON.stringify({method: "username", data: Player.getName()}));
 
 }
 
 // This barely works. It might not work at all :/
 ws.onClose = () => {
     if (Settings.enabled){
-        connected = false
+        connected = false;
         if(!connected && !unload){
-            chat("There has been a connection issue with the websocket. Attempting to reconnect.")
+            chat("There has been a connection issue with the websocket. Attempting to reconnect.");
         } else if (unload){
             return
         }
         else {
-            return chat("Reconnect Failed. Try reloading with /ct reload")
+            return chat("Reconnect Failed. Try reloading with /ct reload");
         }
         connected = true
-        setTimeout(() => ws.reconnect(), 5000)
+        setTimeout(() => ws.reconnect(), 5000);
     }
 }
 
@@ -51,35 +50,34 @@ if(Settings.enabled){
 // Parsing messages sent from the client and sending them to the server
 register('messageSent', (message, event) => {
     if ((!message.startsWith("/") || message.startsWith("/pc") || message.startsWith("/gc") || message.startsWith("/msg") || message.startsWith("/r")) && Settings.enabled) {
-        cancel(event)
-        ws.send(JSON.stringify({ method: "message", data: message }))
+        cancel(event);
+        ws.send(JSON.stringify({ method: "message", data: message }));
     }
 });
 
 register("command", (...args) =>{
     if(args[0] == "testconnection"){
-        ws.send(JSON.stringify({ method: "testConnection" }))
+        ws.send(JSON.stringify({ method: "testConnection" }));
     }
     if (args[0] == "rc"){
-        console.log(args)
-        ws.send(JSON.stringify({method: "message", data: args.join(" ").replace("rc ", "/")}))
+        ws.send(JSON.stringify({method: "message", data: args.join(" ").replace("rc ", "/")}));
     }
     if (args[0] == "settings"){
         Settings.openGUI();
     }
     if (args[0] == "help"){
-        chat("MuteEvasion Commands: \n /muev testconnection - Test the connection to the websocket server. \n /muev rc - Send a message to the server as if it was sent from the client.\n /muev settings - Open the settings GUI. \n /muev setUsername - Lets the other client know what your username is. \n /muev help - Show this message.")
+        chat("MuteEvasion Commands: \n /muev testconnection - Test the connection to the websocket server. \n /muev rc - Send a message to the server as if it was sent from the client.\n /muev settings - Open the settings GUI. \n /muev setUsername - Lets the other client know what your username is. \n /muev help - Show this message.");
     }
     if(args[0] == "setusername"){
-        ws.send(JSON.stringify({method: "configUpdate", data: {username: Player.getName()}}))
+        ws.send(JSON.stringify({method: "configUpdate", data: {username: Player.getName()}}));
     }
     else {
-        chat("Invalid Command. Try /muev help")
+        chat("Invalid Command. Try /muev help");
     }
-}).setName("muev").setTabCompletions(["help", "rc", "testconnection", "settings", "setUsername"])
+}).setName("muev").setTabCompletions(["help", "rc", "testconnection", "settings", "setUsername"]);
 
 register("gameunload", () => {
-    unload = true
-    ws.close()
-    chat("Disconnected from the websocket server.")
-})
+    unload = true;
+    ws.close();
+    chat("Disconnected from the websocket server.");
+});
--- MuteEvasionServer/index.js
+++ MuteEvasionServer/index.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
-const mineflayer = require('mineflayer')
+const mineflayer = require('mineflayer');
 
-const WebSocket = require('ws')
-const wss = new WebSocket.Server({ port: process.env.PORT })
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ port: process.env.PORT });
 
 const bot = mineflayer.createBot({
     viewDistance: 'tiny',
@@ -11,49 +11,49 @@ const bot = mineflayer.createBot({
     host: 'mc.hypixel.io',
     username: process.env.ACCOUNT_NAME,
     auth: process.env.AUTH
-})
+});
 
 let username = "";
 
 // This is awful dont even like try and look at it. I'll add some actually intelligent things to it another time.
 wss.on('connection', function connection(ws) {
   ws.on('message', function message(data) {
-    data = JSON.parse(data)
+    data = JSON.parse(data);
     if (data.method == "testConnection"){
       wss.clients.forEach(function each(client) {
         if (client.readyState === WebSocket.OPEN) {
             client.send(JSON.stringify({method: "message", data: "Connection Successful"}));
-            console.log("Connection Successful")
+            console.log("Connection Successful");
         }
     });
     }
     if (data.method == "message"){
-      bot.chat(`${data.data}`)
+      bot.chat(`${data.data}`);
     }
     if (data.method == "configUpdate"){
-      username = data.data.username
+      username = data.data.username;
     }
   });
 });
 
 bot.on('message', (message) => {
-    console.log(message.toAnsi())
+    console.log(message.toAnsi());
     if (message.toString().includes(`${username} has invited you to join their party!`)){
-      bot.chat(`/p join ${username}`)
+      bot.chat(`/p join ${username}`);
     } 
     if (message.toString().startsWith("From ") || message.toString().startsWith("To ")){
         wss.clients.forEach(function each(client) {
             if (client.readyState === WebSocket.OPEN) {
-                console.log("Sending message")
+                console.log("Sending message");
                 client.send(JSON.stringify({method: "message", data: message.toMotd().toString().replace(/§/g, "&")}));
             }
         });
     }
-})
+});
 
 bot.on("login", () => {
-  bot.chat("§")
-})
+  bot.chat("§");
+});
 
-bot.on('kicked', console.log)
-bot.on('error', console.log)
+bot.on('kicked', console.log);
+bot.on('error', console.log);
